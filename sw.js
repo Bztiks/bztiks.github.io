165be8208d7177fec3d3186921083ab12d88e9e7
北褶tiks <2777973423@qq.com>
2025-05-26 18:15:53 +0800

    Site updated: 2025-05-26 18:15:52

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1748167060542";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1748254547356";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
